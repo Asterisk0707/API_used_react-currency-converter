cf5a0c56886eccfc682027063999d4ab109f4594
Asterisk-_ <101379724+Asterisk0707@users.noreply.github.com>
2025-11-12 22:35:15 +0900

    패키지 수정

--- src/CurrencyFetcher.js
+++ src/CurrencyFetcher.js
@@ -7,9 +7,9 @@ const API_KEY = process.env.REACT_APP_EXCHANGE_RATE_API_KEY;
 const BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
 
 function CurrencyFetcher() {
-  const [rates, setRates] = useState(null);
+  const [rates, setRates] = useState(null); // !!null true 달리는 조건 확인!!
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(null); // String, NULL, Boolean
 
   useEffect(() => {
     const fetchRates = async () => {
